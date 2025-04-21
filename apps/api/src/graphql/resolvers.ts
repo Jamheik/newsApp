b0c720e714965e7fd75c7d64d4da8d6ac0f22fd3
Jamheik <heikkinenjami03@gmail.com>
2025-04-21 14:23:15 +0300

    feat: enhance articles resolver to support search term and update related components

--- apps/api/src/graphql/resolvers.ts
+++ apps/api/src/graphql/resolvers.ts
@@ -1,15 +1,15 @@
-import { Resolver as articleResolver } from './resolvers/articleResolver';
-import { Resolver as articlesResolver } from './resolvers/articlesResolver';
-import { Resolver as feedResolver } from './resolvers/feedResolver';
-import { Resolver as weatherResolver } from './resolvers/weatherResolver';
-import { Resolver as categoriesResolver } from './resolvers/categoriesResolver';
+import { Resolver as articleResolver } from "./resolvers/articleResolver";
+import { Resolver as articlesResolver } from "./resolvers/articlesResolver";
+import { Resolver as feedResolver } from "./resolvers/feedResolver";
+import { Resolver as weatherResolver } from "./resolvers/weatherResolver";
+import { Resolver as categoriesResolver } from "./resolvers/categoriesResolver";
 
 export const resolvers = {
-    Query: {
-        feeds: feedResolver,
-        articles: articlesResolver,
-        article: articleResolver,
-        weather: weatherResolver,
-        categories: categoriesResolver
-    }
+  Query: {
+    feeds: feedResolver,
+    articles: articlesResolver,
+    article: articleResolver,
+    weather: weatherResolver,
+    categories: categoriesResolver,
+  },
 };
